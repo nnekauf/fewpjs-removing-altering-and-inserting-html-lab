b00badbdb0452d6f051a915b9ef7b95ff954be6f
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-04-13 18:19:41 -0400

    commit tree df57c508469cdabafa0b41183c9e4a522ffbeb64

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+// let element = document.querySelector("main#main")
+// // element.className = "main"
+// element.classList.remove("main")
+
+main.remove()
+
+let newHeader = document.createElement('h1')
+// .querySelector("h1#victory");
+newHeader.id = "victory"
+newHeader.innerHTML = "${name} is the champion"
